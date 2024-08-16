1015c35cd4be2336ea2b4435dcfdbd7519dba326
wanderson648 <testepedroca@gmail.com>
2024-08-16 18:32:25 -0300

    cria class User

---
+++ src/main/java/br/wo/api/domain/User.java
@@ -0,0 +1,25 @@
+package br.wo.api.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+}
